c867f173e0546ffc8cd9e008f24137ac70313c58
rsyd29 <brasyid15@gmail.com>
2021-10-27 23:12:43 +0700

    Composing the chips

--- lib/card3.dart
+++ lib/card3.dart
@@ -60,10 +60,85 @@ class Card3 extends StatelessWidget {
                   const SizedBox(
                     height: 30,
                   ),
+                  // 10. You add a Center widget.
+                  Center(
+                    // 11. Wrap is a layout widget that attempts to lay out each of its children adjacent to the previous children. If there’s not enough space, it wraps to the next line.
+                    child: Wrap(
+                      // 12. Place the children as close to the left, i.e. the start, as possible.
+                      alignment: WrapAlignment.start,
+                      // 13. Apply a 12-pixel space between each child.
+                      spacing: 12,
+                      // 14. Add the list of Chip widgets.
+                      children: [
+                        Chip(
+                          label: Text(
+                            'Healthy',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          onDeleted: () {
+                            print('delete');
+                          },
+                        ),
+                        Chip(
+                          label: Text(
+                            'Vegan',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                          onDeleted: () {
+                            print('delete');
+                          },
+                        ),
+                        Chip(
+                          label: Text(
+                            'Carrots',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                        Chip(
+                          label: Text(
+                            'Greens',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                        Chip(
+                          label: Text(
+                            'Wheat',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                        Chip(
+                          label: Text(
+                            'Pescetarian',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                        Chip(
+                          label: Text(
+                            'Mint',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                        Chip(
+                          label: Text(
+                            'Lemongrass',
+                            style: FooderlichTheme.darkTextTheme.bodyText1,
+                          ),
+                          backgroundColor: Colors.black.withOpacity(0.7),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
-            // TODO 7: add Center widget with Chip widget children
+            ),
+
           ],
         ),
       ),
